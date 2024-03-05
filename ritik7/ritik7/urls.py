04c454a9cdeb60c52b87921ae1fef2a2e5b769f4
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-03-05 17:27:19 +0530

    Another way to link or (include) views function inside the file(urls.py) of inner project folder

--- ritik7/ritik7/urls.py
+++ ritik7/ritik7/urls.py
@@ -15,8 +15,41 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
+# This is the another way of or (syntax) to link or (include) the views function in the urls.py file inside the inner project folder
+
+# first synatx has show in project folder ritik6
+
+from course import views as cv
+from fees import views as fs
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('cor/',include([
+#         path('learndjango/',cv.learn_django),
+#         path('learnpy/',cv.learn_python),
+#     ])),
+#     path('fee/',include([
+#         path('feedjango/',fs.fees_django),
+#         path('feepy/',fs.fees_python),
+#     ])),
+# ]
+
+#         OR , 
+
+# This is also another syntax
+
+coursepatterns = [
+    path('learndj/',cv.learn_django),
+    path('learnpy/',cv.learn_python),
+]
+
+feespattern = [
+     path('feedjango/',fs.fees_django),
+        path('feepy/',fs.fees_python),
+]
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cor/',include(coursepatterns)),
+    path('fee/',include(feespattern)),
 ]
