ca822a84a5047d5d70df30b4feea4013ed33c242
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-03-05 14:36:59 +0530

    ritik

--- ritik6/fees/views.py
+++ ritik6/fees/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def fees_django(request):
-    return HttpResponse('hello django')
+    return HttpResponse('300')
 
 def fees_python(request):
-    return HttpResponse('hello python')
+    return HttpResponse('200')
