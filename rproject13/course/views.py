5dcfd2212384e06acabb1758777be8658bcd87c6
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-03-11 22:16:47 +0530

    Render the html file inside the  views file of the applications

---
+++ rproject13/course/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def learn_django(request):
+    # return render(request,'course/course1.html',{'nm':'Django Django'})
+        #  OR,
+    cname = 'Django'
+    duration = '4 months'
+    seats = 10
+    Django_details = {'nm':cname , 'du':duration , 'st':seats}
+    return render(request,'course/course1.html',context=Django_details)
