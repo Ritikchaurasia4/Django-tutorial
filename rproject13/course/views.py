f8edf6e7ef1d0f96ee239f0e08ed720985d6be4d
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-03-11 23:02:49 +0530

    different different filter are use .

--- rproject13/course/views.py
+++ rproject13/course/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import render
 def learn_django(request):
     # return render(request,'course/course1.html',{'nm':'Django Django'})
         #  OR,
-    cname = 'Django'
-    duration = '4 months'
-    seats = 10
-    Django_details = {'nm':cname , 'du':duration , 'st':seats}
-    return render(request,'course/course1.html',context=Django_details)
+    # cname = 'Django'
+    # duration = '4 months'
+    # seats = 10
+    # Django_details = {'nm':cname , 'du':duration , 'st':seats}
+    # return render(request,'course/course1.html',context=Django_details)
+
+    # django_details = {'nm':'Django is awesome , wow'}
+
+    return render(request,'course/course1.html',{'nm':'django is awesome'})
+
+    # return render(request,'course/course1.html',{'nm':False})
