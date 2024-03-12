160f253c3cf28294dd8959f2e8348a81dc57f666
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-03-12 22:26:33 +0530

    if tag are use in django

--- rproject13/course/views.py
+++ rproject13/course/views.py
@@ -19,4 +19,11 @@ def learn_django(request):
     # d = datetime.now()
     # return render(request ,'course/course1.html',{'dt':d})
 
-    return render(request,'course/course1.html',{'p1':56.23421 , 'p2':56.0000 , 'p3':56.370000})
+    # return render(request,'course/course1.html',{'p1':56.23421 , 'p2':56.0000 , 'p3':56.370000})
+
+
+    # if tag
+
+
+    # return render(request,'course/course1.html',{'nm':True})
+      return render(request,'course/course1.html',{'nm':'Django','st':5})
